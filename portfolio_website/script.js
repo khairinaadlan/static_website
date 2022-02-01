d809296b5069f7360d368d64b546c3d1de3872cc
Khairina <khairinaadlan@gmail.com>
2022-02-02 04:06:52 +0900

    add

---
+++ portfolio_website/script.js
@@ -0,0 +1,29 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.navbar-link');
+const progress = document.querySelector('.progress-bar-wrapper');
+const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
+
+window.addEventListener("scroll", () => {
+    if (window.pageYOffset >= navbar.offsetTop) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
+
+    sections.forEach((section, i) => {
+        if (window.pageYOffset >= section.offsetTop - 10) {
+            navbarLinks.forEach(navbarLink => {
+                navbarLink.classList.remove('change');
+            })
+            navbarLinks[i].classList.add("change");
+        }
+    });
+
+    if(window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll('.progress-percent').forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`
+        })
+    }
+});
