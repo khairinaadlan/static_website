018a4d1ca7ec0bf887c02379472b3f6c3bdb2dda
Khairina <khairinaadlan@gmail.com>
2022-02-03 00:40:45 +0900

    portfolio complete

--- portfolio_website/script.js
+++ portfolio_website/script.js
@@ -1,11 +1,15 @@
 const navbar = document.querySelector(".navbar");
 const navbarOffsetTop = navbar.offsetTop;
-const sections = document.querySelectorAll('section');
-const navbarLinks = document.querySelectorAll('.navbar-link');
-const progress = document.querySelector('.progress-bar-wrapper');
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
 const progressBarPercents = [97, 89, 85, 87, 80, 70, 50];
 
 window.addEventListener("scroll", () => {
+    mainFn();
+});
+
+const mainFn = () => {
     if (window.pageYOffset >= navbar.offsetTop) {
         navbar.classList.add("sticky");
     } else {
@@ -14,16 +18,27 @@ window.addEventListener("scroll", () => {
 
     sections.forEach((section, i) => {
         if (window.pageYOffset >= section.offsetTop - 10) {
-            navbarLinks.forEach(navbarLink => {
-                navbarLink.classList.remove('change');
-            })
+            navbarLinks.forEach((navbarLink) => {
+                navbarLink.classList.remove("change");
+            });
             navbarLinks[i].classList.add("change");
         }
     });
 
-    if(window.pageYOffset + window.innerHeight >= progress.offsetTop) {
-        document.querySelectorAll('.progress-percent').forEach((el, i) => {
-            el.style.width = `${progressBarPercents[i]}%`
-        })
+    if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+        document.querySelectorAll(".progress-percent").forEach((el, i) => {
+            el.style.width = `${progressBarPercents[i]}%`;
+            // progress-percent from css change width according to array PBPercent
+            el.previousElementSibling.firstElementChild.textContent =
+                progressBarPercents[i];
+        });
     }
-});
+};
+
+mainFn();
+// want to run the code once before scroll to avoid unexpected behaviour, place inside callback function and global
+
+window.addEventListener('resize', () => {
+    window.location.reload();
+})
+
